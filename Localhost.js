46a9f62f2a66913dc6d65dc707900147e0781313
kingsharp21 <kingsharpn3@gmail.com>
2023-12-16 14:56:25 +0000

    changes for job

--- Localhost.js
+++ Localhost.js
@@ -1,53 +1,53 @@
-// singup form
-window.onload = () => {
-  this.sessionStorage.setItem("email", "");
-  this.sessionStorage.setItem("password", "");
-};
+// // singup form
+// window.onload = () => {
+//   this.sessionStorage.setItem("email", "");
+//   this.sessionStorage.setItem("password", "");
+// };
 
-var input = document.getElementsByTagName("input");
-var register = document.getElementById("register");
-var form = document.querySelector("form");
-form.onsubmit = () => {
-  return false;
-};
+// var input = document.getElementsByTagName("input");
+// var register = document.getElementById("register");
+// var form = document.querySelector("form");
+// form.onsubmit = () => {
+//   return false;
+// };
 
-register.onclick = () => {
-  if (input[0].value == "" && input[1].value != "") {
-    if (
-      input[0].value == sessionStorage.getItem("email") &&
-      input[1].value == sessionStorage.getItem("password")
-    ) {
-      form.onsubmit = () => {
-        return 1;
-      };
-      document.cookie = "email" + input[0].value;
-      document.cookie = "password" + input[1].value;
-    } else {
-      if (input[0].value != sessionStorage.getItem("email")) {
-        input[0].nextElementSibling.textContent = "Email NOT match";
-        setTimeout(() => {
-          input[0].nextElementSibling.textContent = "";
-        }, 2000);
-      }
-      if (input[1].value != sessionStorage.getItem("password")) {
-        input[1].nextElementSibling.textContent = "Password NOT match";
-        setTimeout(() => {
-          input[1].nextElementSibling.textContent = "";
-        }, 2000);
-      }
-    }
-  } else {
-    if (input[0].value == "") {
-      input[0].nextElementSibling.textContent = "Email is empty";
-      setTimeout(() => {
-        input[0].nextElementSibling.textContent = "";
-      }, 2000);
-    }
-    if (input[1].value == "") {
-      input[1].nextElementSibling.textContent = "Password is empty";
-      setTimeout(() => {
-        input[1].nextElementSibling.textContent = "";
-      }, 2000);
-    }
-  }
-};
+// register.onclick = () => {
+//   if (input[0].value == "" && input[1].value != "") {
+//     if (
+//       input[0].value == sessionStorage.getItem("email") &&
+//       input[1].value == sessionStorage.getItem("password")
+//     ) {
+//       form.onsubmit = () => {
+//         return 1;
+//       };
+//       document.cookie = "email" + input[0].value;
+//       document.cookie = "password" + input[1].value;
+//     } else {
+//       if (input[0].value != sessionStorage.getItem("email")) {
+//         input[0].nextElementSibling.textContent = "Email NOT match";
+//         setTimeout(() => {
+//           input[0].nextElementSibling.textContent = "";
+//         }, 2000);
+//       }
+//       if (input[1].value != sessionStorage.getItem("password")) {
+//         input[1].nextElementSibling.textContent = "Password NOT match";
+//         setTimeout(() => {
+//           input[1].nextElementSibling.textContent = "";
+//         }, 2000);
+//       }
+//     }
+//   } else {
+//     if (input[0].value == "") {
+//       input[0].nextElementSibling.textContent = "Email is empty";
+//       setTimeout(() => {
+//         input[0].nextElementSibling.textContent = "";
+//       }, 2000);
+//     }
+//     if (input[1].value == "") {
+//       input[1].nextElementSibling.textContent = "Password is empty";
+//       setTimeout(() => {
+//         input[1].nextElementSibling.textContent = "";
+//       }, 2000);
+//     }
+//   }
+// };
